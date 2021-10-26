f4a115217ca6d8c4ddb912f41b6ecb2e9eff01cb
cpalumbo <carumbo@Carl-NB.misco.com>
2021-10-26 18:30:59 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,33 @@
+//import logo from './logo.svg';
+import React, { Component } from 'react';
+import './App.css';
+import Navbar from './components/layout/Navbar';
+//import UserItem from './components/users/UserItem';
+import Users from './components/users/Users';
+import axios from 'axios';
+
+class App extends Component {
+  state = {
+    users: [],
+    loading: false,
+  };
+
+  async componentDidMount() {
+    this.setState({ loading: true });
+    const res = await axios.get('https://api.github.com/users');
+    this.setState({ users: res.data, loading: false });
+  }
+
+  render() {
+    return (
+      <div className='App'>
+        <Navbar />
+        <div className='container'>
+          <Users loading={this.state.loading} users={this.state.users} />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
