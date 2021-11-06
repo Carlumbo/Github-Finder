e2d5cbdf910ce578419f0d44b4fb4d76c3b7ea9b
carlumbo <carlpalumbo1@gmail.com>
2021-11-06 17:03:46 -0400

    added alert component

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -8,19 +8,33 @@ class Search extends Component {
 
   static propTypes = {
     searchUsers: PropTypes.func.isRequired,
+    clearUsers: PropTypes.func.isRequired,
+    showClear: PropTypes.bool.isRequired,
+    setAlert: PropTypes.func.isRequired,
+    //justCarl: PropTypes.func.isRequired,
   };
 
   onChange = (e) => {
+    //you set it uop this way so it doenst rely on the subject change, it relies on the event being passed as a varible passed on where it occured
     this.setState({ [e.target.name]: e.target.value });
   };
 
   onSubmit = (e) => {
+    //arrow funciton makes it so I do not have to bind in the form setup
     e.preventDefault();
-    this.props.searchUsers(this.state.text);
-    this.setState({ text: '' });
+    //you must do this prevent default otherwise itll try to submit to a file
+    if (this.state.text === '') {
+      this.props.setAlert('Please enter something', 'light');
+    } else {
+      this.props.searchUsers(this.state.text);
+      //must be passwe up to app.js
+      //state is passed in to the searchUsers function
+      this.setState({ text: '' });
+    }
   };
 
   render() {
+    const { showClear, clearUsers } = this.props;
     return (
       <div>
         <form className='form' onSubmit={this.onSubmit}>
@@ -28,6 +42,7 @@ class Search extends Component {
             type='text'
             name='text'
             placeholder='Search Users...'
+            //input field will be available for
             value={this.state.text}
             onChange={this.onChange}
           />
@@ -37,6 +52,19 @@ class Search extends Component {
             className='btn btn-dark btn-block'
           />
         </form>
+        {/*
+        <button
+          className='btn btn-light btn-block'
+          onClick={this.props.justCarl}
+        >
+          Just Carl
+        </button>
+        */}
+        {showClear && (
+          <button className='btn btn-light btn-block' onClick={clearUsers}>
+            Clear
+          </button>
+        )}
       </div>
     );
   }
