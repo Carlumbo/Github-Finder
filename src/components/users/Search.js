3ce78cceee0e4edf168af3d28019a44101394880
carlumbo <carlpalumbo1@gmail.com>
2021-11-16 17:05:06 -0800

    Added Context and cleaned up app.js

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,7 +1,11 @@
-import React, { useState } from 'react';
-import PropTypes from 'prop-types';
+import React, { useState, useContext } from 'react';
+import GithubContext from '../../context/github/githubContext';
+import AlertContext from '../../context/alert/alertContext';
 
-const Search = ({ searchUsers, showClear, clearUsers, setAlert }) => {
+const Search = () => {
+  const githubContext = useContext(GithubContext);
+  const alertContext = useContext(AlertContext);
+
   const [text, setText] = useState('');
 
   const onChange = (e) => {
@@ -12,12 +16,11 @@ const Search = ({ searchUsers, showClear, clearUsers, setAlert }) => {
   const onSubmit = (e) => {
     //arrow funciton makes it so I do not have to bind in the form setup
     e.preventDefault();
-    //you must do this prevent default otherwise itll try to submit to a file
     if (text === '') {
-      setAlert('Please enter something', 'light');
+      alertContext.setAlert('Please enter something', 'light');
     } else {
-      searchUsers(text);
-      //must be passwe up to app.js
+      githubContext.searchUsers(text);
+      //must be passed up to app.js
       //state is passed in to the searchUsers function
       setText('');
     }
@@ -47,21 +50,16 @@ const Search = ({ searchUsers, showClear, clearUsers, setAlert }) => {
           Just Carl
         </button>
         */}
-      {showClear && (
-        <button className='btn btn-light btn-block' onClick={clearUsers}>
+      {githubContext.users.length > 0 && (
+        <button
+          className='btn btn-light btn-block'
+          onClick={githubContext.clearUsers}
+        >
           Clear
         </button>
       )}
     </div>
   );
-};
-
-Search.propTypes = {
-  searchUsers: PropTypes.func.isRequired,
-  clearUsers: PropTypes.func.isRequired,
-  showClear: PropTypes.bool.isRequired,
-  setAlert: PropTypes.func.isRequired,
-  //justCarl: PropTypes.func.isRequired,
 };
 
 export default Search;
